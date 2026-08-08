9d2dcbb97e100ea735ab41c7a0b1a931ff357351
engineeraether377-dotcom <engineeraether377@gmail.com>
2026-08-08 21:30:03 +0500

    fix(types): add quick global JSX intrinsic typings for r3f elements to unblock build

---
+++ src/types/global.d.ts
@@ -0,0 +1,15 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    // Common r3f elements used in this project
+    ambientLight: any;
+    directionalLight: any;
+    pointLight: any;
+    spotLight: any;
+    primitive: any;
+    group: any;
+    mesh: any;
+    perspectiveCamera: any;
+    orthographicCamera: any;
+    // Add more r3f intrinsics here as needed
+  }
+}
